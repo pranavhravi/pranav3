f022008d7fd4e2f06f4f6aad88c0883fa967d438
pranavhravi <pranavhravi34@gmail.com>
2025-07-17 17:37:27 +0530

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -27,39 +27,40 @@ document.addEventListener('visibilitychange', function () {
 });
 
 // fetch projects start
-function getProjects() {
-    return fetch("projects.json")
-        .then(response => response.json())
-        .then(data => {
-            return data;
-        });
-}
+// Disabled dynamic project fetching to prevent overwriting static HTML
+// function getProjects() {
+//     return fetch("projects.json")
+//         .then(response => response.json())
+//         .then(data => {
+//             return data;
+//         });
+// }
 
-function showProjects(projects) {
-    let projectsContainer = document.querySelector(".work .box-container");
-    let projectsHTML = "";
-    projects.forEach(project => {
-        projectsHTML += `
-        <div class="grid-item ${project.category}">
-            <div class="box tilt" style="width: 380px; margin: 1rem">
-                <img draggable="false" src="./assets/images/projects/${project.image}.jpg" alt="project" />
-                <div class="content">
-                    <div class="tag">
-                        <h3>${project.name}</h3>
-                    </div>
-                    <div class="desc">
-                        <p>${project.desc}</p>
-                    </div>
-                </div>
-            </div>
-        </div>`;
-    });
-    projectsContainer.innerHTML = projectsHTML;
-}
+// function showProjects(projects) {
+//     let projectsContainer = document.querySelector(".work .box-container");
+//     let projectsHTML = "";
+//     projects.forEach(project => {
+//         projectsHTML += `
+//         <div class="grid-item ${project.category}">
+//             <div class="box tilt" style="width: 380px; margin: 1rem">
+//                 <img draggable="false" src="./assets/images/projects/${project.image}.jpg" alt="project" />
+//                 <div class="content">
+//                     <div class="tag">
+//                         <h3>${project.name}</h3>
+//                     </div>
+//                     <div class="desc">
+//                         <p>${project.desc}</p>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>`;
+//     });
+//     projectsContainer.innerHTML = projectsHTML;
+// }
 
-//getProjects().then(data => {
-  //  showProjects(data);
-//});
+// getProjects().then(data => {
+//     showProjects(data);
+// });
 // fetch projects end
 
 // Start of Tawk.to Live Chat
